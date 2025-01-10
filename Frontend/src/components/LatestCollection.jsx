45bf9ecac3e8baeb58c5f0f634adab5f547007bd
sowbaranigakps-04 <sowbaranigakps04@gmail.com>
2025-01-10 08:05:23 +0530

    Collections

--- Frontend/src/components/LatestCollection.jsx
+++ Frontend/src/components/LatestCollection.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
+    const [latestProducts, setLatestProducts] = useState([]);
+    
+    useEffect(() =>{
+      setLatestProducts(products.slice(0,10));
+
+    },[])
     
   return (
     <div className='my-10'>
@@ -15,6 +22,15 @@ const LatestCollection = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex tempora ipsa harum magnam dolores ad tenetur hic. Dolorum ducimus aliquam ullam fuga quam. Error earum reprehenderit molestias incidunt odio architecto.
         </p>
       </div>
+    {/*Rendering Products */}
+    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+      {
+        latestProducts.map((item,index)=>(
+          <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+        ))
+      }
+    </div>
+    
       
     </div>
   )
--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
  import { createContext } from "react";
  import { products } from "../assets/assets";
+ import React, { useState } from 'react';
 
  export const ShopContext = createContext();
 
@@ -7,9 +8,13 @@
 
     const currency = '₹';
     const delivery_fee = 40;
+    const [search,setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false)
+
 
     const value = {
-        products, currency, delivery_fee
+        products, currency, delivery_fee,
+        search,setSearch,showSearch,setShowSearch
     }
     return(
         <ShopContext.Provider value={value}>
